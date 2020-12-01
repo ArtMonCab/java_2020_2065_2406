c4dd477e8c9e32418153e8cadf1e9aa3f0fa660d
amont <amontanez@gmail.com>
2020-12-01 10:29:49 +0100

    Singleton

--- supermercado/src/com/ipartek/formacion/supermercado/accesodatos/ProductoDaoFreeMap.java
+++ supermercado/src/com/ipartek/formacion/supermercado/accesodatos/ProductoDaoFreeMap.java
@@ -4,7 +4,7 @@ import java.util.TreeMap;
 
 import com.ipartek.formacion.supermercado.modelos.Producto;
 
-public class ProductoDaoFreeMap implements Dao{
+public class ProductoDaoFreeMap implements Dao<Producto>{
 	
 	private static TreeMap<Long, Producto> productos = new TreeMap<>();
 	
@@ -29,25 +29,24 @@ public class ProductoDaoFreeMap implements Dao{
 	
 
 	@Override
-	public Iterable obtenerTodos() {
+	public Iterable<Producto> obtenerTodos() {
 		return productos.values();
 	}
 
 	@Override
-	public Object obtenerPorId(Long id) {
+	public Producto obtenerPorId(Long id) {
 		return productos.get(id);
 	}
 
 	@Override
-	public void crear(Object objetos) {
+	public void crear(Producto producto) {
 		Long id = productos.size() == 0 ? 1L : productos.lastKey() + 1L;
 		producto.setId(id);
 		productos.put(id, producto);
-		
 	}
 
 	@Override
-	public void modificar(Object objeto) {
+	public void modificar(Producto producto) {
 		productos.put(producto.getId(), producto);
 		
 	}
