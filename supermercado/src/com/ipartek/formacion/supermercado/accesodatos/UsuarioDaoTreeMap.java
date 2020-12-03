618294261428e4f29e97079731ac675a4ba939f0
Curso JAVA <amontanez@gmail.com>
2020-12-03 12:21:48 +0100

    Dao de usuario con operación específica de obtenerPorEmail

--- supermercado/src/com/ipartek/formacion/supermercado/accesodatos/UsuarioDaoTreeMap.java
+++ supermercado/src/com/ipartek/formacion/supermercado/accesodatos/UsuarioDaoTreeMap.java
@@ -1,35 +1,39 @@
 package com.ipartek.formacion.supermercado.accesodatos;
 
-import java.math.BigDecimal;
 import java.util.TreeMap;
 
-import com.ipartek.formacion.supermercado.modelos.Producto;
-import com.ipartek.formacion.supermercado.modelos.Usuario;
 import com.ipartek.formacion.supermercado.modelos.Usuario;
 
-public class UsuarioDaoTreeMap implements Dao<Usuario>{
-	
+public class UsuarioDaoTreeMap implements Dao<Usuario> {
 	private static TreeMap<Long, Usuario> usuarios = new TreeMap<>();
-	
-	static {
-		usuarios.put(1L, new Usuario(1L, "pepito@perez.com", "qwerty"));
-		usuarios.put(2L, new Usuario(2L, "perico@delospalotes.com", "qwerty"));
 
+	static {
+		usuarios.put(1L, new Usuario(1L, "javier@lete.com", "contra"));
+		usuarios.put(2L, new Usuario(2L, "pepe@perez.com", "perez"));
 	}
 
 	// SINGLETON
 
+	// Ponemos privado el constructor por defecto para que nadie pueda crear
+	// instancias de esta clase de forma libre
+	// Con esto evitamos la posibilidad de que nadie haga new de esta clase (salvo
+	// esta clase en sí misma)
 	private UsuarioDaoTreeMap() {
 	}
 
+	// Creamos el único objeto que va a existir de este tipo
 	private static UsuarioDaoTreeMap INSTANCIA = new UsuarioDaoTreeMap();
 
+	// Creamos un método público que de acceso a la única instancia disponible
+	// Desde otras clases deberemos hacer
+	// ProductoDaoTreeMap dao = ProductoDaoTreeMap.getInstancia();
+	// o mejor
+	// Dao<Producto> dao = ProductoDaoTreeMap.getInstancia();
 	public static UsuarioDaoTreeMap getInstancia() {
 		return INSTANCIA;
 	}
 
 	// FIN SINGLETON
-
 	@Override
 	public Iterable<Usuario> obtenerTodos() {
 		return usuarios.values();
@@ -41,21 +45,44 @@ public class UsuarioDaoTreeMap implements Dao<Usuario>{
 	}
 
 	@Override
-	public void crear(Usuario producto) {
+	public void crear(Usuario usuario) {
 		Long id = usuarios.size() == 0 ? 1L : usuarios.lastKey() + 1L;
-		producto.setId(id);
-		usuarios.put(id, producto);
+		usuario.setId(id);
+		usuarios.put(id, usuario);
 	}
 
 	@Override
-	public void modificar(Usuario producto) {
-		usuarios.put(producto.getId(), producto);
+	public void modificar(Usuario usuario) {
+		usuarios.put(usuario.getId(), usuario);
 	}
 
 	@Override
 	public void eliminar(Long id) {
 		usuarios.remove(id);
 	}
-
-
+	
+	// Método específico de este DAO
+	
+	public Usuario obtenerPorEmail(String email) {
+		for(Usuario usuario: usuarios.values()) {
+			if(usuario.getEmail().equals(email)) {
+				return usuario;
+			}
+		}
+		
+		return null;
+	}
+	
+//	public Usuario obtenerPorEmail(String email) {
+//		Usuario resultado = null;
+//		
+//		for(Usuario usuario: usuarios.values()) {
+//			if(usuario.getEmail().equals(email)) {
+//				resultado = usuario;
+//				break;
+//			}
+//		}
+//		
+//		return resultado;
+//	}
 }
