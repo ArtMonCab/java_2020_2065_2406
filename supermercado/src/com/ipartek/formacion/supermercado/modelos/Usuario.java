97d0753cdc7b1ffb3fc5bf5ff3e2de1fdccf4f40
Curso JAVA <amontanez@gmail.com>
2020-12-11 11:55:14 +0100

    Modelo de usuario

--- supermercado/src/com/ipartek/formacion/supermercado/modelos/Usuario.java
+++ supermercado/src/com/ipartek/formacion/supermercado/modelos/Usuario.java
@@ -4,30 +4,33 @@ public class Usuario {
 	private Long id;
 	private String email, password;
 	
-	
-	
 	public Usuario(Long id, String email, String password) {
 		super();
 		this.id = id;
 		this.email = email;
 		this.password = password;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -73,10 +76,4 @@ public class Usuario {
 	public String toString() {
 		return "Usuario [id=" + id + ", email=" + email + ", password=" + password + "]";
 	}
-	
-	
-	
-	
-	
-
 }
