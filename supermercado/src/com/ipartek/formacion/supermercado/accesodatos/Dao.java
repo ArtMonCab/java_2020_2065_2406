c231834815c24d1f7b6cbb28a38db7d1697ca515
amont <amontanez@gmail.com>
2021-01-18 08:26:49 +0100

    Recuperación de borrados

--- supermercado/src/com/ipartek/formacion/supermercado/accesodatos/Dao.java
+++ supermercado/src/com/ipartek/formacion/supermercado/accesodatos/Dao.java
@@ -16,13 +16,16 @@ public interface Dao<T> {
 	default void crear(T objeto){
 		throw new AccesoDatosException("MÉTODO NO IMPLEMENTADO");
 	}
-	
-
+	default T crearYObtener(T objeto){
+		throw new AccesoDatosException("MÉTODO NO IMPLEMENTADO");
+	}
 	default void modificar(T objeto){
 		throw new AccesoDatosException("MÉTODO NO IMPLEMENTADO");
 	}
-	
 	default void eliminar(Long id){
+		throw new AccesoDatosException("MÉTODO NO IMPLEMENTADO");
+	}
+	default void recuperar(Long id){
 		throw new AccesoDatosException("MÉTODO NO IMPLEMENTADO");
 	}
 }
